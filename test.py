24d5e51eae06b2e2adfd3b8109cd3a005ad60e18
kyoji63 <bk.greenk.c22@icloud.com>
2024-11-11 21:01:05 +0900

    add token

--- test.py
+++ test.py
@@ -5,6 +5,8 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
+TOKEN = "MTMwNDcxMDgzMjYyOTg3NDc0OQ.Gr2N8d.rQIcqgYYjx_ZfQbewRu3D1gD4rDI0n7Lb35UuE"
+
 random_contents=[
     "なにもかんがえたくない",
     "かんがえてみようかな",
